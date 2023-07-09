5ccfcd4ea39ff6d22a287e8848fa07343fee8cf2
iamthepoe <luca1901p@gmail.com>
2023-07-09 14:58:16 -0300

    add namespace

--- HttpServer.cs
+++ HttpServer.cs
@@ -2,55 +2,58 @@ using System.Net.Sockets;
 using System.Net;
 using System.Text;
 
-class HttpServer
+namespace SimpleHttpServer
 {
-    private TcpListener Controller { get; set; }
-    private int Port { get; set; }
-    private int RequestsLength { get; set; }
-
-    HttpServer(int port = 8080)
+    public class HttpServer
     {
-        try
-        {
-            this.Controller = new TcpListener(IPAddress.Parse("127.0.0.1"), this.Port);
-            this.Controller.Start();
-            Console.WriteLine($"Server running at http://localhost:{this.Port}");
-            Task httpServerTask = Task.Run(() => AwaitRequests());
-            httpServerTask.GetAwaiter().GetResult();
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine($"Error on start server at port {this.Port}\n{e.Message}");
-        }
-    }
+        private TcpListener Controller { get; set; }
+        private int Port { get; set; }
+        private int RequestsLength { get; set; }
 
-    private async Task AwaitRequests()
-    {
-        while (true)
+        public HttpServer(int port = 8080)
         {
-            Socket connection = await this.Controller.AcceptSocketAsync();
-            this.RequestsLength++;
-            Task task = Task.Run(() => ProcessRequest(connection, this.RequestsLength));
-        }
-    }
-
-    private void ProcessRequest(Socket connection, int requestNumber)
-    {
-        Console.WriteLine($"Processing request #{requestNumber}");
-        if (connection.Connected)
-        {
-            byte[] requestBytes = new Byte[1024];
-            connection.Receive(requestBytes, requestBytes.Length, 0);
-            string requestText = Encoding.UTF8.GetString(requestBytes)
-              .Replace((char)0, ' ').Trim();
-
-            if (requestText.Length > 0)
+            try
             {
-                Console.WriteLine($"\n{requestText}\n");
-                connection.Close();
+                this.Controller = new TcpListener(IPAddress.Parse("127.0.0.1"), this.Port);
+                this.Controller.Start();
+                Console.WriteLine($"Server running at http://localhost:{this.Port}");
+                Task httpServerTask = Task.Run(() => AwaitRequests());
+                httpServerTask.GetAwaiter().GetResult();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error on start server at port {this.Port}\n{e.Message}");
             }
         }
 
-        Console.WriteLine($"\nRequest #{requestNumber} finalized.");
+        private async Task AwaitRequests()
+        {
+            while (true)
+            {
+                Socket connection = await this.Controller.AcceptSocketAsync();
+                this.RequestsLength++;
+                Task task = Task.Run(() => ProcessRequest(connection, this.RequestsLength));
+            }
+        }
+
+        private void ProcessRequest(Socket connection, int requestNumber)
+        {
+            Console.WriteLine($"Processing request #{requestNumber}");
+            if (connection.Connected)
+            {
+                byte[] requestBytes = new Byte[1024];
+                connection.Receive(requestBytes, requestBytes.Length, 0);
+                string requestText = Encoding.UTF8.GetString(requestBytes)
+                  .Replace((char)0, ' ').Trim();
+
+                if (requestText.Length > 0)
+                {
+                    Console.WriteLine($"\n{requestText}\n");
+                    connection.Close();
+                }
+            }
+
+            Console.WriteLine($"\nRequest #{requestNumber} finalized.");
+        }
     }
 }
