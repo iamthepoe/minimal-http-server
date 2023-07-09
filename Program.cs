39574477d3370e3b2075ccc30b6f14097d789f49
iamthepoe <luca1901p@gmail.com>
2023-07-09 14:59:36 -0300

    create new server

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SimpleHttpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var httpServer = new HttpServer();
+        }
+    }
+}
